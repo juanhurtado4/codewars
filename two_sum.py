5758c0f3459bd4cb802aae3a8518c6e1645101ad
juan hurtado <juanhurtado4@outlook.com>
2017-09-23 23:35:34 -0700

    Created main and add_two function

---
+++ two_sum.py
@@ -0,0 +1,36 @@
+'''
+Problem description:
+Given an array of integers return the indexes of two integers that
+add up to the target number.
+Example:
+arr = [2, 7, 11, 20]
+tager_num = 9
+check = arr[0] + arr[1] # equals 9 because 2 + 7 = 9
+return (0, 1)
+'''
+
+def add_two(array, target):
+    '''
+    Function receives an array and an Int.
+    Function finds the indexes of two numbers from the array that add up to
+    target number.
+    Returns tuple of indexes.
+    '''
+    for index, each in enumerate(array):
+        for second_index, second_each in enumerate(array):
+            if second_index == 0:
+                continue
+            elif (second_each + each) == target:
+                    result_indexes = (array.index(each), array.index(second_each))
+                    return (result_indexes)
+
+
+def main():
+    array = input("Please enter array: ")
+    array = array.split(',')
+    [int(number) for number in array]
+    target = int(input("Please enter target number: "))
+    print(add_two(array, target))
+
+if __name__ == '__main__':
+    main()
