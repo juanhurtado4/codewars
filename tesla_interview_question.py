393c97a71db6ca74f46246541ae8fae4057dd9c1
juan hurtado <juanhurtado4@outlook.com>
2017-10-26 01:11:33 -0700

    Added doc string

--- tesla_interview_question.py
+++ tesla_interview_question.py
@@ -2,6 +2,11 @@
 # Do not add a space at the end of the character
 # Return a string e.g. "hello world" -> "h e l l o w o r l d"
 def tespace(user_input):
+    '''
+    Function adds a space after every character
+    user_input: String
+    Returns String
+    '''
     new_str = ''
     for char in user_input:
         if char == ' ':
