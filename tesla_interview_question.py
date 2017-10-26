5812a25eed5bc8de5cab877659290748decd782c
juan hurtado <juanhurtado4@outlook.com>
2017-10-26 01:10:23 -0700

    Finished tesla question

---
+++ tesla_interview_question.py
@@ -0,0 +1,11 @@
+# Given a string add a space after every character
+# Do not add a space at the end of the character
+# Return a string e.g. "hello world" -> "h e l l o w o r l d"
+def tespace(user_input):
+    new_str = ''
+    for char in user_input:
+        if char == ' ':
+            new_str += char
+            continue
+        new_str += char + ' '
+    return new_str[:-1]
